851136f321f665ede861346d22da5ddc9d364d92
Magara.m <magara.m@clicknext.com>
2020-11-13 14:08:50 +0700

    update app

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const rooms = { };
 const io = require('socket.io')(require("http").createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}));
+}).listen(8081));
 
 
 // noinspection JSUnresolvedFunction
